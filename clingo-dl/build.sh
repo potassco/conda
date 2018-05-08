d2e984427f1518554a6c7164a0b07c2762921772
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2018-05-08 18:03:13 +0200

    remove c compiler options because it is not used

--- clingo-dl/build.sh
+++ clingo-dl/build.sh
@@ -5,7 +5,6 @@ cd build
 
 cmake .. \
     -DCMAKE_CXX_COMPILER="${CXX}" \
-    -DCMAKE_C_COMPILER="${CC}" \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_INSTALL_LIBDIR="lib" \
     -DCLINGODL_BUILD_WITH_SYSTEM_CLINGO=On \
