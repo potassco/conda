adc1886a0f021d323839bc63bf0dbf0199c9b4f0
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-04-04 14:29:34 +0200

    update to use clingcon4 dev

--- clingcon/bld.bat
+++ clingcon/bld.bat
@@ -3,7 +3,6 @@ mkdir build
 cmake -G "%CMAKE_GENERATOR%" -H. -Bbuild ^
     -DCMAKE_CXX_COMPILER="%CXX%" ^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
-    -DCLINGCON_BUILD_WITH_SYSTEM_CLINGO=On ^
     -DCMAKE_INSTALL_BINDIR="."
 
 cmake --build build --config Release
