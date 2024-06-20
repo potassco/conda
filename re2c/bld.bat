586823f615bb51fae021186836af7db3ff949a28
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-06-20 15:44:17 +0200

    update

--- re2c/bld.bat
+++ re2c/bld.bat
@@ -1,10 +1,10 @@
 mkdir re2c_build
 
-cmake -G "Visual Studio 15 2017" -S. -Bre2c_build ^
+cmake -G "Ninja" -S. -Bre2c_build ^
     -DCMAKE_CXX_COMPILER="%CXX%" ^
     -DCMAKE_C_COMPILER="%CC%" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
 
-cmake --build re2c_build --config release
-cmake --build re2c_build --config release --target install
+cmake --build re2c_build
+cmake --build re2c_build --target install
