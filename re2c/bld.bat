f2737b0beb3d09a6018f03ba836c0a9774c680d2
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-06-20 15:12:54 +0200

    build re2c

--- re2c/bld.bat
+++ re2c/bld.bat
@@ -1,2 +1,10 @@
-xcopy . "%LIBRARY_BIN%" /S /Y
-del "%LIBRARY_BIN%"\bld.bat
+mkdir re2c_build
+
+cmake -G "Ninja" -S. -B"re2c_build" ^
+    -DCMAKE_CXX_COMPILER="%CXX%" ^
+    -DCMAKE_C_COMPILER="%CC%" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+
+cmake --build re2c_build
+cmake --build re2c_build --target install
--- re2c/build.sh
+++ re2c/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} CXX="${CXX}" CC="${CC}"
-make -j${CPU_COUNT}
-make install
+mkdir re2c_build
 
+cmake -G "Ninja" -S. -B"re2c_build" \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_BUILD_TYPE=Release
+
+cmake --build re2c_build
+cmake --build re2c_build --target install
