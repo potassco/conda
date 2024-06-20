b42ba25a98daa2720f8a91bafabb3da5badc3008
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-06-20 16:23:58 +0200

    one last chance

--- re2c/bld.bat
+++ re2c/bld.bat
@@ -2,15 +2,11 @@ echo on
 
 mkdir re2c_build
 
-echo "**** configure ****"
-
 cmake -G "Ninja" -S . -B re2c_build ^
     -DCMAKE_CXX_COMPILER="%CXX%" ^
     -DCMAKE_C_COMPILER="%CC%" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
-
-echo "**** build ****"
 
 cmake --build re2c_build
 cmake --build re2c_build --target install
